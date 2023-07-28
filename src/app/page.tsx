3654212223a91781434d414e8a1866cc06242a69
Carles <cpcastells@gmail.com>
2023-07-28 20:26:46 +0200

    Create AnimatedText component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout/Layout";
 import Image from "next/image";
 import room from "../../public/images/room.webp";
+import AnimatedText from "@/components/AnimatedText/AnimatedText";
 
 export default function Home() {
   return (
@@ -15,12 +16,15 @@ export default function Home() {
               className="max-w-[700px] w-full h-auto"
             ></Image>
           </div>
-          <div className="w-1/2">
-            <h1> &lt;Carles Pueyo/&gt;</h1>
-            <p>
+          <div className="w-1/2 flex flex-col items-center self-center">
+            <AnimatedText
+              text="<Carles Pueyo/>"
+              className="text-6xl text-left"
+            ></AnimatedText>
+            <p className="my-4 text-base font-medium">
               As a full-stack developer, my portfolio features a variety of
               projects, with a primary focus on React development. Explore my
-              work to see the creative applications I&aposve built. Don&apost
+              work to see the creative applications I&apos;ve built. Don&apos;t
               hesitate to reach out for collaboration or any questions you may
               have.
             </p>
