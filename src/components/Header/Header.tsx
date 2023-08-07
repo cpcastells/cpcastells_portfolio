bd47ca49536496bce1369b41990a449452f33c82
Carles <cpcastells@gmail.com>
2023-08-07 15:18:56 +0200

    Add break point to menu button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,21 +18,21 @@ const Header = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
       <button
-        className="flex flex-col justify-center items-center"
+        className="flex-col justify-center items-center hidden lg:flex"
         onClick={handleClick}
       >
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm ${
+          className={`bg-dark dark:bg-light transition-all duration-300 block h-0.5 w-6 rounded-sm ${
             isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
           }`}
         ></span>
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm my-0.5 ${
+          className={`bg-dark dark:bg-light transition-all duration-300 block h-0.5 w-6 rounded-sm my-0.5 ${
             isOpen ? "opacity-0" : "opacity-1"
           }`}
         ></span>
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm ${
+          className={`bg-dark dark:bg-light transition-all duration-300 block h-0.5 w-6 rounded-sm ${
             isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
           }`}
         ></span>
