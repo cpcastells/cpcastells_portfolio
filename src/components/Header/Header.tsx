c55b95517970bde9c5f1aee1264ee2c3185c4b25
Carles <cpcastells@gmail.com>
2023-08-07 15:09:32 +0200

    Add isOpen state to menu button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,13 +9,33 @@ import useTheme from "@/hooks/useTheme";
 
 const Header = () => {
   const [mode, setMode] = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = (): void => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
-      <button className="flex flex-col justify-center items-center">
-        <span className="bg-dark dark:bg-light block h-0.5 w-6 rounded-sm -translate-y-0.5"></span>
-        <span className="bg-dark dark:bg-light block h-0.5 w-6 rounded-sm my-0.5"></span>
-        <span className="bg-dark dark:bg-light block h-0.5 w-6 rounded-sm translate-y-0.5"></span>
+      <button
+        className="flex flex-col justify-center items-center"
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-1"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          }`}
+        ></span>
       </button>
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
