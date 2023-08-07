614eb9698e18ca6708c3f7fa81ed9f08ce83c527
Carles <cpcastells@gmail.com>
2023-08-07 22:47:37 +0200

    Add media queries to Project and FeaturedProject components

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -15,9 +15,12 @@ const Project = ({ github, img, link, title, type }: FeaturedProjectProps) => {
   return (
     <article
       className="w-full flex flex-col item-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative
-    dark:bg-dark dark:border-light"
+    dark:bg-dark dark:border-light xs:p-4"
     >
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[1.5rem] bg-dark dark:bg-light" />
+      <div
+        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[1.5rem] bg-dark dark:bg-light 
+      md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]"
+      />
       <Link
         href={link}
         target="_blank"
@@ -26,7 +29,7 @@ const Project = ({ github, img, link, title, type }: FeaturedProjectProps) => {
         <Image src={img} alt={title} className="w-full h-auto" />
       </Link>
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary dark:text-primaryDark font-medium text-xl">
+        <span className="text-primary dark:text-primaryDark font-medium text-xl lg:text-lg md:text-base">
           {type}
         </span>
         <Link
@@ -34,17 +37,19 @@ const Project = ({ github, img, link, title, type }: FeaturedProjectProps) => {
           target="_blank"
           className="hover:underline underline-offset-2"
         >
-          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">
+            {title}
+          </h2>
         </Link>
         <div className="w-full mt-2 flex items-center justify-between">
           <Link
             href={github}
             target="_blank"
-            className="text-lg font-semibold hover:underline underline-offset-2"
+            className="text-lg font-semibold hover:underline underline-offset-2 md:text-base"
           >
             Visit
           </Link>
-          <Link href={github} target="_blank" className="w-8">
+          <Link href={github} target="_blank" className="w-8 md:w-6">
             <GithubIcon />
           </Link>
         </div>
